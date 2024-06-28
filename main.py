d4c5bf75e7a3af8bfecdbd5e4743984cc8a5e14d
AEgor18 <aegor246@mail.ru>
2024-06-28 13:27:02 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+text = 'python'
+items = 100
+
 headers = {
     'Host': 'hh.ru',
     'User-Agent': 'Yandex'
 }
-url = 'https://hh.ru/search/vacancy?text=python&items_on_page=100'
-request = requests.get(url, headers=headers)
-# print(request.status_code)
-soup = bs(request.text, 'html.parser')
-# paginator = soup.find('span', {'class': ''})
-#
+url = f'https://hh.ru/search/vacancy?&text={text}&items_on_page={items}'
+def hh():
+    request = requests.get(url, headers=headers)
+    # print(request.status_code)
+    soup = bs(request.text, 'html.parser')
+    paginator = soup.find_all('span', {'class': 'pager-item-not-in-short-range'})
+
+    pages=[]
+
+    for page in paginator:
+        pages.append(int(page.find('a').text))
+
+    return pages[-1]
+
+max_page = hh()
+
+def hh_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f'{url}&page=0', headers=headers)
+        print(result.status_code)
+        soup = bs(result.text, 'html.parser')
+        results = soup.find_all('div',  {'class': 'vacancy-search-item__card'})
+        for result in results:
+            print(result.find('a').text)
+
+    return jobs
+hh_jobs(max_page)
